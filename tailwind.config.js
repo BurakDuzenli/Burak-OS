f75961e4e962a4579ab26848d91747db57196195
Burak Düzenli <duzenliburak0@gmail.com>
2024-12-06 17:48:59 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dogica: ["Dogica", "sans-serif"],
+        "dogica-bold": ["Dogicabold", "sans-serif"],
+        "dogica-pixel": ["Dogicapixel", "sans-serif"],
+        "dogica-pixel-bold": ["Dogicapixelbold", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
