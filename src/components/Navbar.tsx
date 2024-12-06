bea1ff7ab0b8a26c51b479a25516d887e847397d
Burak Düzenli <duzenliburak0@gmail.com>
2024-12-06 23:59:54 +0300

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,37 @@
-import textBookImage from "../assets/book-svgrepo-com.svg";
+import React from "react";
+import textBookImage from "../assets/book-album-svgrepo-com.svg";
 import searchIcon from "../assets/search-svgrepo-com.svg";
-
+import projectsIcon from "../assets/robot-svgrepo-com.svg";
+import chatIcon from "../assets/chat-round-svgrepo-com.svg";
 function Navbar() {
+  function SystemDetails(
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void {}
+
   return (
     <div className="items-center justify-between flex m-3 p-3 bg-black border border-white w-auto box-border">
       <div className="text-white font-dogica-bold">BURAK-OS</div>
       <div className="flex space-x-6 items-center">
-        <div className="w-6 h-6">
+        <div className="w-5 h-5">
           <button>
             <img src={searchIcon} alt="" />
           </button>
         </div>
-        <div className="w-8 h-8">
-          <button>
+        <div className="w-5 h-5">
+          <button onClick={SystemDetails}>
             <img src={textBookImage} alt="" />
           </button>
         </div>
-        <div className="w-4 h-4 bg-white"></div>
-        <div className="w-4 h-4 bg-white"></div>
+        <div className="w-5 h-5">
+          <button>
+            <img src={projectsIcon} alt="" />
+          </button>
+        </div>
+        <div className="w-5 h-5 ">
+          <button>
+            <img src={chatIcon} alt="" />
+          </button>
+        </div>
       </div>
     </div>
   );
