5f2215f195435de6299c9aad783fd4a3aded8f52
Burak Düzenli <duzenliburak0@gmail.com>
2024-12-06 20:27:08 +0300

    hmhm

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
-import React from "react";
 import textBookImage from "../assets/book-svgrepo-com.svg";
+import searchIcon from "../assets/search-svgrepo-com.svg";
 
 function Navbar() {
   return (
     <div className="items-center justify-between flex m-3 p-3 bg-black border border-white w-auto box-border">
       <div className="text-white font-dogica-bold">BURAK-OS</div>
-      <div className="flex space-x-4 items-center">
-        <div className="w-4 h-4 bg-white"></div>
-        <div className="w-4 h-4 bg-white"></div>
+      <div className="flex space-x-6 items-center">
+        <div className="w-6 h-6">
+          <button>
+            <img src={searchIcon} alt="" />
+          </button>
+        </div>
+        <div className="w-8 h-8">
+          <button>
+            <img src={textBookImage} alt="" />
+          </button>
+        </div>
         <div className="w-4 h-4 bg-white"></div>
         <div className="w-4 h-4 bg-white"></div>
       </div>
